38e3e4dc6b10db7eb42c81618a4bd41b25a88b1c
Vasanth <68737119+vazand@users.noreply.github.com>
2024-01-24 09:41:41 +0530

    React routes inintial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Header.js";
+import Footer from "./Footer.js";
+import Nav from "./Nav";
+import Home from "./Home.js";
+import NewPost from "./NewPost.js";
+import PostPage from "./PostPage.js";
+import About from "./About.js";
+import Missing from "./Missing.js";
+import { Link, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="m-4">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home </Link>
+          </li>
+          <li>
+            <Link to={"/about"}>About </Link>
+          </li>
+          <li>
+            <Link to="/newPost">NewPost </Link>
+          </li>
+          <li>
+            <Link to={"/post"}>Post </Link>
+          </li>
+        </ul>
+      </nav>
+      {/*
+      The Routes and route will help us to reach the page from url
+      the Link used to show us the same in the front`
+      */}
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/newPost" element={<NewPost />} />
+        <Route path="/post" element={<PostPage />} />
+      </Routes>
+      {/* <Header />
+      <Nav />
+      <Home />
+      <NewPost />
+      <PostPage />
+      <About />
+      <Missing />
+      <Footer /> */}
     </div>
   );
 }
